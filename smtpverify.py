96054e6a3b9b00cbad6f00813b9ec4bd1a718a76
c0g <cgardi203@caledonian.ac.uk>
2019-06-19 23:29:12 +0100

    Added verify script

---
+++ smtpverify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import socket
+from concurrent.futures import ThreadPoolExecutor
+
+if len(sys.argv) != 3:
+    print(
+        "usage: smtpverify.py [IP] [USER LIST]")
+    exit(1)
+
+
+ip = sys.argv[1]
+users_list = sys.argv[2]
+
+try:
+    with(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.settimeout(3)
+        s.connect((ip, 25))
+        s.recv(1024) # Recieve the banner
+        with (open(users_list, 'r')) as users:
+            for username in users.readlines():
+                s.send('VRFY %s' % username)
+                response = s.recv(1024)
+                print(response.decode('ascii'))
+except:
+    print('Error connecting to %s' % ip)
